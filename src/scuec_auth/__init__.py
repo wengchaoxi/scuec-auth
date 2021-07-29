2434cb4346d1fc517ebc080f16d8144b329a8dc9
WengChaoxi <352120190@qq.com>
2021-07-29 15:33:54 +0800

    update

--- src/scuec_auth/__init__.py
+++ src/scuec_auth/__init__.py
@@ -12,5 +12,5 @@ from .utils import debug, random_string, random_bytes, encrypt_aes, decrypt_aes
 from .session import Session, SessionCache
 from .auth import simple_headers, SCUECAuth
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __all__ = ['compat_str', 'compat_bytes', 'debug', 'random_string', 'random_bytes', 'encrypt_aes', 'decrypt_aes', 'Session', 'SessionCache', 'simple_headers', 'SCUECAuth']
