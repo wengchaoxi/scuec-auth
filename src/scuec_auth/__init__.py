52df62047ad75e2177a90ffaff0c3eea7dc94a69
WengChaoxi <352120190@qq.com>
2021-07-29 16:51:59 +0800

    update

--- src/scuec_auth/__init__.py
+++ src/scuec_auth/__init__.py
@@ -8,9 +8,9 @@
     :license: MIT, see LICENSE for more details.
 """
 from ._compat import compat_str, compat_bytes
-from .utils import debug, random_string, random_bytes, encrypt_aes, decrypt_aes, time_counter
+from .utils import error, debug, random_string, random_bytes, encrypt_aes, decrypt_aes, time_counter
 from .session import Session, SessionCache
 from .auth import simple_headers, SCUECAuth
 
 __version__ = '1.1.0'
-__all__ = ['compat_str', 'compat_bytes', 'debug', 'random_string', 'random_bytes', 'encrypt_aes', 'decrypt_aes', 'time_counter', 'Session', 'SessionCache', 'simple_headers', 'SCUECAuth']
+__all__ = ['compat_str', 'compat_bytes', 'error', 'debug', 'random_string', 'random_bytes', 'encrypt_aes', 'decrypt_aes', 'time_counter', 'Session', 'SessionCache', 'simple_headers', 'SCUECAuth']
